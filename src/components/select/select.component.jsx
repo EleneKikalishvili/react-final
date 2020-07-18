4223a07f3f2e05f4a1880d2d0303c26407c03cb9
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-07-18 14:29:41 +0400

    added spinner to detailed card

--- src/components/select/select.component.jsx
+++ src/components/select/select.component.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getBreeds, getDetailedInfo } from "../../api";
 
 import DetailedCard from "../detailed-card/detailed-card.component";
+import withSpinner from "../spinner/spinner.component";
 
 import styles from "./select.module.css";
 
@@ -10,6 +11,9 @@ function Select() {
   const [info, setInfo] = useState([]);
   const [image, setImage] = useState("");
   const [selected, setSelected] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const CardWithSpinner = withSpinner(DetailedCard);
 
   useEffect(() => {
     let isSubscribed = true;
@@ -31,10 +35,12 @@ function Select() {
 
   const handleChange = (e) => {
     getDetailedInfo(e.target.value).then((res) => {
+      setLoading(true);
       setInfo(res.data[0].breeds);
       setImage(res.data[0].url);
       setSelected(true);
     });
+    setTimeout(() => setLoading(false), 1000);
   };
 
   return (
@@ -52,13 +58,14 @@ function Select() {
         {!selected ? (
           <h3 className={styles.msg}>Nothing To Show</h3>
         ) : (
-          <DetailedCard
+          <CardWithSpinner
             name={info[0].name}
             image={image}
             origin={info[0].origin}
             temperament={info[0].temperament}
             description={info[0].description}
             life={info[0].life_span}
+            isLoading={loading}
           />
         )}
       </div>
